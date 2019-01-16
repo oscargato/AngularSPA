03bc5e231ed95e75bd87ee11640db8e1a590450e
Oscar Mejia <oscardongato@gmail.com>
2019-01-16 08:23:32 -0400

    Commit Base de SPA

---
+++ src/app/components/carrusel/carrusel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carrusel',
+  templateUrl: './carrusel.component.html'
+})
+
+export class CarruselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
